12fff9e18bf6c94155e8b1c31beb80db147a5f8e
gitwuxian36 <101049573+gitwuxian36@users.noreply.github.com>
2022-03-06 21:27:12 +0800

    Add files via upload

---
+++ file/wxaccess_log_analysis (3).py
@@ -0,0 +1,11 @@
+def application(environment, start_response):
+    """
+    The main WSGI Application. Doesn't really do anything
+    since we're benchmarking the servers, not this code :)
+    """
+
+    start_response(
+        '200 OK',  # Status
+        [('Content-type', 'text/plain'), ('Content-Length', '2')]  # Headers
+    )
+    return ['OK']
